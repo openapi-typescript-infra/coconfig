750bf7c3bb6f24588c19999089bbd8f6939191af
Max Metral <max.metral@sesamecare.com>
2023-05-26 00:59:13 -0400

    fix(esm): remove jest esm settings

--- src/jest.ts
+++ src/jest.ts
@@ -1,10 +1,8 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 export const jestConfig: JestConfigWithTsJest = {
-  extensionsToTreatAsEsm: ['.ts'],
   verbose: true,
   preset: 'ts-jest',
-  resolver: 'ts-jest-resolver',
   testEnvironment: 'node',
   testRegex: '(\\.|/)(test|spec)\\.[jt]sx?$',
 };
